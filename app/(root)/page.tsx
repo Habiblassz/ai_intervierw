a5c0ce3e5fa0a515d27afb8c66413866d1cafe83
Lawal Habib <habiblass2001@gmail.com>
2025-04-02 10:22:52 +0100

    Showed interviews generated on the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import {
+	getCurrentUser,
+	getInterviewsByUserId,
+	getLatestInterviews,
+} from "@/lib/actions/auth.action";
 import Link from "next/link";
 
-export default function Home() {
+async function Home() {
+	const user = await getCurrentUser();
+
+	const [userInterviews, latestInterviews] = await Promise.all([
+		await getInterviewsByUserId(user?.id!),
+		await getLatestInterviews({ userId: user?.id! }),
+	]);
+
+	const hasPassesdInterviews = userInterviews?.length! > 0;
+	const hasUpcomingInterviews = latestInterviews?.length! > 0;
+
 	return (
 		<>
 			<section className="card-cta">
@@ -27,20 +41,29 @@ export default function Home() {
 			<section className="flex flex-col gap-6 mt-8">
 				<h2>Your interviews</h2>
 				<div className="interviews-section">
-					{dummyInterviews.map((interview) => (
-						<InterviewCard {...interview} key={interview.id} />
-					))}
+					{hasPassesdInterviews ? (
+						userInterviews?.map((interview) => (
+							<InterviewCard {...interview} key={interview.id} />
+						))
+					) : (
+						<p>You haven&apos;t taken any inteviews yet</p>
+					)}
 				</div>
 			</section>
 			<section className="flex flex-col gap-6 mt-8">
 				<h2>Take an Interview</h2>
 				<div className="interviews-section">
-					{dummyInterviews.map((interview) => (
-						<InterviewCard {...interview} key={interview.id} />
-					))}
-					{/* <p>You haven&apos;t taken any inteviews yet</p> */}
+					{hasUpcomingInterviews ? (
+						latestInterviews?.map((interview) => (
+							<InterviewCard {...interview} key={interview.id} />
+						))
+					) : (
+						<p>There are no new interviews available</p>
+					)}
 				</div>
 			</section>
 		</>
 	);
 }
+
+export default Home;
